fddf0821f272898d333ed085c405604e03850335
Home_PC_arch_btw <tienpham102vp@gmail.com>
2024-10-25 00:43:41 +0700

    MVP version complete

--- lua/chadrc.lua
+++ lua/chadrc.lua
@@ -1,17 +1,75 @@
--- This file needs to have same structure as nvconfig.lua 
+-- This file needs to have same structure as nvconfig.lua
 -- https://github.com/NvChad/ui/blob/v3.0/lua/nvconfig.lua
--- Please read that file to know all available options :( 
+-- Please read that file to know all available options :(
 
 ---@type ChadrcConfig
 local M = {}
 
 M.base46 = {
-	theme = "onedark",
+  theme = "ayu_dark",
 
-	-- hl_override = {
-	-- 	Comment = { italic = true },
-	-- 	["@comment"] = { italic = true },
-	-- },
+  -- hl_override = {
+  -- 	Comment = { italic = true },
+  -- 	["@comment"] = { italic = true },
+  -- },
+}
+
+M.nvdash = {
+  load_on_startup = true,
+  header = {
+    "",
+    "",
+    "",
+    "",
+    "",
+    " ▄▄▄▄▄▄▄ ▄▄▄ ▄▄▄▄▄▄▄ ▄▄    ▄ ▄▄▄▄▄▄▄ ",
+    "█       █   █       █  █  █ █       █",
+    "█▄     ▄█   █    ▄▄▄█   █▄█ █    ▄  █",
+    "  █   █ █   █   █▄▄▄█       █   █▄█ █",
+    "  █   █ █   █    ▄▄▄█  ▄    █    ▄▄▄█",
+    "  █   █ █   █   █▄▄▄█ █ █   █   █    ",
+    "  █▄▄▄█ █▄▄▄█▄▄▄▄▄▄▄█▄█  █▄▄█▄▄▄█    ",
+    "",
+    "",
+    "",
+  },
+  buttons = {
+    { txt = "  Find File", keys = "ff", cmd = "Telescope find_files" },
+    { txt = "  Recent Files", keys = "fo", cmd = "Telescope oldfiles" },
+    { txt = "󰈭  Find Word", keys = "fw", cmd = "Telescope live_grep" },
+    { txt = "󱥚  Themes", keys = "th", cmd = ":lua require('nvchad.themes').open()" },
+    { txt = "  Mappings", keys = "ch", cmd = "NvCheatsheet" },
+    { txt = "󰅗  Quit", keys = "qq", cmd = ":q" },
+
+    { txt = "─", hl = "NvDashLazy", no_gap = true, rep = true },
+
+    {
+      txt = function()
+        local stats = require("lazy").stats()
+        local ms = math.floor(stats.startuptime) .. " ms"
+        return "  Loaded " .. stats.loaded .. "/" .. stats.count .. " plugins in " .. ms
+      end,
+      hl = "NvDashLazy",
+      no_gap = true,
+    },
+
+    { txt = "─", hl = "NvDashLazy", no_gap = true, rep = true },
+  },
+}
+
+M.ui = {
+  telescope = {
+    style = "bordered",
+  },
+  statusline = {
+    theme = "minimal", -- default/vscode/vscode_colored/minimal
+    -- default/round/block/arrow separators work only for default statusline theme
+    -- round and block will work for minimal theme only
+    separator_style = "round",
+  },
+  cheatsheet = {
+    theme = "simple",
+  },
 }
 
 return M
--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -3,22 +3,33 @@ require("nvchad.configs.lspconfig").defaults()
 
 local lspconfig = require "lspconfig"
 
--- EXAMPLE
-local servers = { "html", "cssls" }
+local servers = { "bashls", "cmake", "pylsp", "clangd" }
 local nvlsp = require "nvchad.configs.lspconfig"
 
--- lsps with default config
+-- overwrite default "gr" keymap
+local overwrite_on_attach = function(client, bufnr)
+  nvlsp.on_attach(client, bufnr)
+  vim.keymap.set("n", "gr", "<cmd> Telescope lsp_references<cr>", { buffer = bufnr })
+end
+
 for _, lsp in ipairs(servers) do
   lspconfig[lsp].setup {
-    on_attach = nvlsp.on_attach,
+    on_attach = overwrite_on_attach,
     on_init = nvlsp.on_init,
     capabilities = nvlsp.capabilities,
   }
 end
 
--- configuring single server, example: typescript
--- lspconfig.ts_ls.setup {
---   on_attach = nvlsp.on_attach,
---   on_init = nvlsp.on_init,
---   capabilities = nvlsp.capabilities,
--- }
+-- manually set up LSP lua_ls
+lspconfig.lua_ls.setup {
+  on_attach = overwrite_on_attach,
+  on_init = nvlsp.on_init,
+  capabilities = nvlsp.capabilities,
+  settings = {
+    Lua = {
+      diagnostics = {
+        globals = { "vim" },
+      },
+    },
+  },
+}
