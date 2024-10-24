d9e09dcd78c77f52875f5f25da943b7f6afcae3a
Home_PC_arch_btw <tienpham102vp@gmail.com>
2024-10-24 22:39:26 +0700

    add useful keymaps

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -7,4 +7,25 @@ local map = vim.keymap.set
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
 
--- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
+-- Resize window using <ctrl> arrow keys
+map("n", "<C-Up>", "<cmd>resize +2<cr>", { desc = "Increase Window Height" })
+map("n", "<C-Down>", "<cmd>resize -2<cr>", { desc = "Decrease Window Height" })
+map("n", "<C-Left>", "<cmd>vertical resize -2<cr>", { desc = "Decrease Window Width" })
+map("n", "<C-Right>", "<cmd>vertical resize +2<cr>", { desc = "Increase Window Width" })
+
+-- useful edit keymaps
+map("x", "<leader>p", [["_dP]], { desc = "Primeagen Copy" })
+map("n", "<C-d>", "<C-d>zz", { desc = "Scroll plus zz" })
+map("n", "<C-u>", "<C-u>zz", { desc = "Scroll plus zz" })
+map("n", "n", "nzzzv")
+map("n", "N", "Nzzzv")
+map("v", "J", ":m '>+1<CR>gv=gv", { desc = "Move Line Up" })
+map("v", "K", ":m '<-2<CR>gv=gv", { desc = "Move Line Down" })
+
+-- disable diagnostic
+map(
+  "n",
+  "<leader>ud",
+  ":lua vim.diagnostic.enable(not vim.diagnostic.is_enabled())<CR>",
+  { desc = "Toggle Diagnostic" }
+)
