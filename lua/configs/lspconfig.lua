09be63e77e361dd0a91fc2345fefcaccab7e1eca
arch_wsl_workNB <tienpham102vp@gmail.com>
2025-02-14 16:48:22 +0700

    codeium plugin add

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -3,7 +3,7 @@ require("nvchad.configs.lspconfig").defaults()
 
 local lspconfig = require "lspconfig"
 
-local servers = { "bashls", "cmake", "pylsp", "clangd" }
+local servers = { "bashls", "pylsp", "csharp_ls", "clangd", "neocmake" }
 local nvlsp = require "nvchad.configs.lspconfig"
 
 for _, lsp in ipairs(servers) do
@@ -13,17 +13,3 @@ for _, lsp in ipairs(servers) do
     capabilities = nvlsp.capabilities,
   }
 end
-
--- manually set up LSP lua_ls
-lspconfig.lua_ls.setup {
-  on_attach = nvlsp.on_attach(client, bufnr),
-  on_init = nvlsp.on_init,
-  capabilities = nvlsp.capabilities,
-  settings = {
-    Lua = {
-      diagnostics = {
-        globals = { "vim" },
-      },
-    },
-  },
-}
---
+++ lua/plugins/codium.lua
@@ -0,0 +1,21 @@
+-- return {
+--   "Exafunction/codeium.nvim",
+--   cmd = "Codeium",
+--   event = "InsertEnter",
+--   build = ":Codeium Auth",
+--   opts = {
+--     enable_cmp_source = vim.g.ai_cmp,
+--     virtual_text = {
+--       enabled = not vim.g.ai_cmp,
+--       key_bindings = {
+--         accept = "<C-y>", -- handled by nvim-cmp / blink.cmp
+--         next = "<M-]>",
+--         prev = "<M-[>",
+--       },
+--     },
+--   },
+-- }
+return {
+  "Exafunction/codeium.vim",
+  event = "BufEnter",
+}
